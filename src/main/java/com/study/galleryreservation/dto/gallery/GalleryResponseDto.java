529d3aae32d6f906676b3bd4c5c5aa0a6078c2b3
phyun0920 <topjun1@nate.com>
2026-03-30 22:02:57 +0900

    update

--- src/main/java/com/study/galleryreservation/dto/gallery/GalleryResponseDto.java
+++ src/main/java/com/study/galleryreservation/dto/gallery/GalleryResponseDto.java
@@ -1,8 +1,31 @@
 package com.study.galleryreservation.dto.gallery;
 
+import com.study.galleryreservation.domain.gallery.Gallery;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter @Builder
 public class GalleryResponseDto {
+    private final Long id;
+    private final String name;
+    private final String location;
+    private final String description;
+    private final Integer capacity;
+    private final boolean is_active;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    // 엔티티를 DTO로 변환하는 생성자
+    public GalleryResponseDto(Gallery entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.location = entity.getLocation();
+        this.description = entity.getDescription();
+        this.capacity = entity.getCapacity();
+        this.is_active = entity.is_active();
+        this.createdAt = entity.getCreatedAt();
+        this.updatedAt = entity.getUpdatedAt();
+    }
 }
