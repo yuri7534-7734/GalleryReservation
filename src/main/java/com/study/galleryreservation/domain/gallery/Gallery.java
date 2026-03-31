9109c5fd2911d14d93013656659f9638c4a383f5
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-03-31 18:18:08 +0900

    update

--- src/main/java/com/study/galleryreservation/domain/gallery/Gallery.java
+++ src/main/java/com/study/galleryreservation/domain/gallery/Gallery.java
@@ -2,15 +2,40 @@ package com.study.galleryreservation.domain.gallery;
 
 import jakarta.persistence.*;
 import lombok.*;
+
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "gallery")
-@Getter @Builder @AllArgsConstructor @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class Gallery {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "capacity")
+    private Integer capacity;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 
 }
--- src/main/java/com/study/galleryreservation/dto/gallery/GalleryCreateRequestDto.java
+++ src/main/java/com/study/galleryreservation/dto/gallery/GalleryCreateRequestDto.java
@@ -1,8 +1,27 @@
 package com.study.galleryreservation.dto.gallery;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.study.galleryreservation.domain.gallery.Gallery;
+import lombok.*;
 
-@Getter @Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GalleryCreateRequestDto {
+    private String name;
+    private String location;
+    private String description;
+    private Integer capacity;
+    private boolean isActive;
+
+    // Service 레이어에서 엔티티로 변환할 때 사용
+    public Gallery toEntity() {
+        return Gallery.builder()
+                .name(name)
+                .location(location)
+                .description(description)
+                .capacity(capacity)
+                .isActive(isActive)
+                .build();
+    }
 }
