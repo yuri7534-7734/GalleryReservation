c230537e5aae975278dd0fd1d5ba52bd096bd816
phyun0920 <topjun1@nate.com>
2026-03-31 10:08:12 +0900

    update phyun

--- src/main/java/com/study/galleryreservation/dto/gallery/GalleryUpdateRequestDto.java
+++ src/main/java/com/study/galleryreservation/dto/gallery/GalleryUpdateRequestDto.java
@@ -1,8 +1,19 @@
 package com.study.galleryreservation.dto.gallery;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-@Getter @Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GalleryUpdateRequestDto {
+    // 수정하고 싶은 필드만 정의
+    private String name;
+    private String location;
+    private String description;
+    private Integer capacity;
+    private boolean is_active;
 }
