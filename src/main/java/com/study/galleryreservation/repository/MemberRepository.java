2b1c0addeea2a10fbc66cbbf05e85abb71cfe892
phyun0920 <topjun1@nate.com>
2026-03-31 18:57:32 +0900

    update

--- src/main/java/com/study/galleryreservation/repository/MemberRepository.java
+++ src/main/java/com/study/galleryreservation/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.study.galleryreservation.repository;
 import com.study.galleryreservation.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
 }
