fc0de622ef1a78e43bee18c25c117dfa68576a9b
phyun0920 <topjun1@nate.com>
2026-03-31 18:54:25 +0900

    update

--- src/main/java/com/study/galleryreservation/dto/member/MemberJoinRequestDto.java
+++ src/main/java/com/study/galleryreservation/dto/member/MemberJoinRequestDto.java
@@ -1,8 +1,18 @@
 package com.study.galleryreservation.dto.member;
 
+import com.study.galleryreservation.domain.member.MemberRole;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter @Setter
+@NoArgsConstructor
 public class MemberJoinRequestDto {
+    private String username;
+    private String password;
+    private String email;
+    private MemberRole role;
+    private LocalDateTime created_at;
 }
