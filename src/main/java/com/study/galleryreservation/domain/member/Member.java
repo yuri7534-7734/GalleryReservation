b9831a292d9fe3b0219e4852614d44824855fd9a
yuri7534-7734 <yuri7534@naver.com>
2026-04-02 18:30:46 +0900

    4월2일 마무리

--- src/main/java/com/study/galleryreservation/domain/member/Member.java
+++ src/main/java/com/study/galleryreservation/domain/member/Member.java
@@ -3,8 +3,6 @@ package com.study.galleryreservation.domain.member;
 import com.study.galleryreservation.domain.reservation.Reservation;
 import com.study.galleryreservation.domain.todo.Todo;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -25,11 +23,11 @@ public class Member {
     private Long id;
 
     @Column(name = "username", nullable = false, unique = true, length = 50)
-    @Size(min = 4, max = 20)
+    //@Size(min = 4, max = 20)
     public String username;
 
     @Column(name = "password", nullable = false, length = 255)
-    @Size(min = 8, max = 20)
+    //@Size(min = 8, max = 20)
     public String password;
 
     @Column(name = "email", nullable = false, unique = true, length = 100)
