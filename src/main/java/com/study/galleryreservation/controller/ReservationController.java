872eced6fee48ed1435e85067f6ecea72685aac3
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-04-02 17:54:46 +0900

    사용자 예약 상세 페이지 기능 구현
    소개글 및 일부 기능은 추후의 추가해야함

--- src/main/java/com/study/galleryreservation/controller/ReservationController.java
+++ src/main/java/com/study/galleryreservation/controller/ReservationController.java
@@ -1,6 +1,9 @@
 package com.study.galleryreservation.controller;
 
+import com.study.galleryreservation.domain.gallery.Gallery;
+import com.study.galleryreservation.domain.reservation.Reservation;
 import com.study.galleryreservation.domain.session.SessionUser;
+import com.study.galleryreservation.dto.gallery.GalleryUpdateRequestDto;
 import com.study.galleryreservation.dto.reservation.ReservationCreateRequestDto;
 import com.study.galleryreservation.dto.reservation.ReservationResponseDto;
 import com.study.galleryreservation.repository.GalleryRepository;
@@ -21,6 +24,7 @@ public class ReservationController {
     private final GalleryRepository galleryRepository;
     private final ReservationService reservationService;
 
+    // 예약 페이지로 이동
     @GetMapping("/form")
     public String getForm(Model model) {
         model.addAttribute("reservationCreateRequestDto", new ReservationCreateRequestDto());
@@ -28,6 +32,7 @@ public class ReservationController {
         return "reservation/form";
     }
 
+    // 예약하기
     @PostMapping("/form")
     public String postForm(@ModelAttribute ReservationCreateRequestDto requestDto,
                            HttpSession session) {
@@ -36,6 +41,7 @@ public class ReservationController {
         return "redirect:/reservation/list";
     }
 
+    // 예약 리스트 패이지
     @GetMapping("/list")
     public String reservationList(Model model, HttpSession session) {
         SessionUser sessionUser = (SessionUser) session.getAttribute("user");
@@ -44,6 +50,17 @@ public class ReservationController {
         return "reservation/list";
     }
 
+    // 예약 상세 페이지
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable Long id, HttpSession session, Model model) {
+        SessionUser sessionUser = (SessionUser) session.getAttribute("user");
+        Reservation reservation = reservationService.findByIdAndEmail(id, sessionUser.getEmail());
+        model.addAttribute("reservation", ReservationResponseDto.from(reservation));
+        model.addAttribute("gallery", reservation.getGallery());
+        return "reservation/reservation-detail";
+    }
+
+    // 예약 취소하기
     @PostMapping("/cancel/{id}")
     public String cancel(@PathVariable Long id, HttpSession session) {
         SessionUser sessionUser = (SessionUser) session.getAttribute("user");
