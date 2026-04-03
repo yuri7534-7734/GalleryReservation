a1b9e626e12bcc0fc49c8446e07b838c5d34a58d
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-04-03 11:52:56 +0900

    예약페이지 새 예약 버튼 삭제

--- src/main/java/com/study/galleryreservation/controller/ReservationController.java
+++ src/main/java/com/study/galleryreservation/controller/ReservationController.java
@@ -22,25 +22,7 @@ public class ReservationController {
     private final GalleryRepository galleryRepository;
     private final GalleryService galleryService;
     private final ReservationService reservationService;
-
-
-    // 예약 페이지로 이동
-    @GetMapping("/form")
-    public String getForm(Model model) {
-        model.addAttribute("reservationCreateRequestDto", new ReservationCreateRequestDto());
-        model.addAttribute("galleries", galleryRepository.findAll());
-        return "reservation/form";
-    }
-
-    // 예약하기
-    @PostMapping("/form")
-    public String postForm(@ModelAttribute ReservationCreateRequestDto requestDto,
-                           HttpSession session) {
-        SessionUser sessionUser = (SessionUser) session.getAttribute("user");
-        galleryService.save(requestDto, sessionUser.getEmail());
-        return "redirect:/reservation/list";
-    }
-
+    
     // 예약 리스트 패이지
     @GetMapping("/list")
     public String reservationList(@RequestParam(defaultValue = "0") int page, Model model, HttpSession session) {
