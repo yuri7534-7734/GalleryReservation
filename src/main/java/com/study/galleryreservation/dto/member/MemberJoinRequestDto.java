9bc010bcc9e37fdf35743636f9a8f6abdad15238
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-04-03 16:46:11 +0900

    Update MemberJoinRequestDto.java

--- src/main/java/com/study/galleryreservation/dto/member/MemberJoinRequestDto.java
+++ src/main/java/com/study/galleryreservation/dto/member/MemberJoinRequestDto.java
@@ -18,7 +18,6 @@ public class MemberJoinRequestDto {
     @Size(min = 5, max = 20, message = "아이디는 5자 이상 20자 이하로 입력해 주세요.")
     private String username;
 
-
     @NotBlank(message = "비밀번호를 입력해 주세요.")
     @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해 주세요.")
     private String password;
